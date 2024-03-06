f83771bf6839bf4e95c7d3d17bbe94000620e5d8
Umair Khan <m.umairkhan.co@proton.me>
2024-03-06 16:38:14 +0500

    ADDED server actions

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -0,0 +1,21 @@
+import { deleteBoard } from "@/actions/delete-board";
+import { Button } from "@/components/ui/button";
+
+interface BoardProps {
+  title: string;
+  id: string;
+}
+
+export const Board = ({ title, id }: BoardProps) => {
+  const deleteBoardWithId = deleteBoard.bind(null, id);
+  return (
+    <form className="flex items-center gap-x-2" action={deleteBoardWithId}>
+      <p>
+        <div>Board Name: {title}</div>
+      </p>
+      <Button variant="destructive" size="sm" type="submit">
+        Delete
+      </Button>
+    </form>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,20 @@
+import { db } from "@/lib/db";
 import React from "react";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board title={board.title} id={board.id} key={board.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
